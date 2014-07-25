6d90ae364e5808fab3aa4754404ebb92ec2f729d
Sameer Dhar <avielus@gmail.com>
2014-07-24 23:07:03 -0400

    Finished problem 2, started problem 3

--- problems/clojure/2.clj
+++ problems/clojure/2.clj
@@ -1,3 +1,14 @@
 ; By considering the terms in the Fibonacci sequence
 ; whose values do not exceed four million,
 ; find the sum of the even-valued terms.
+
+(def fib-seq
+  ((fn rfib [a b]
+     (lazy-seq (cons a (rfib b (+ a b)))))
+   0 1))
+
+(defn even? [number]
+  (= 0(mod number 2)))
+
+
+(reduce + (filter even? (take-while #(< % 4000000) fib-seq)))
